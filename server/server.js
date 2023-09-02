90f6b9b1977ece6ef0fbde98b18ac5f1dbc31a1a
Kaitlyn Leitherer <kaitlynleitherer@gmail.com>
2023-09-02 12:07:23 -0400

    september 2nd changes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+//const PORT = 3000;
 
+const cors = require('cors');
+
+// Use CORS middleware before your routes
+app.use(cors());
+
+/*
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+*/
 
+/*
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+});
+*/
+app.listen(3001, () => {
+  console.log('Server runs!');
 });
